0ebd8e9c3398dd63649ab79f5052fbf5a25147c4
PhoVic14 <91669693+PhoVic14@users.noreply.github.com>
2023-03-09 16:08:12 +0100

    ajout de la structure

---
+++ VictorPholien_Projet_2048/VictorPholien_Projet_2048/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace VictorPholien_Projet_2048
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+
+        static void PlacerNombre()
+        {
+
+        }
+
+
+        static void BougerNombre()
+        {
+
+        }
+
+
+        static void AdditionnerNombre()
+        {
+
+        }
+
+        static void creeMatrice()
+        {
+
+        }
+    }
+}
